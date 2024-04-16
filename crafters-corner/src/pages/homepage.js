a59f3043264a3865bc0f2d27631b18a10943e8cb
JaxGenius <stasio.ignasz1@gmail.com>
2024-04-16 01:09:07 +0100

    Working on creating a logged on version of the site

--- crafters-corner/src/pages/homepage.js
+++ crafters-corner/src/pages/homepage.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Homepage() {
+function Homepage({ isLoggedIn, username }) {
   return (
     <div>
       <header>
         <Link to="/"><img src="logo.png" alt="Logo" /></Link>
         <nav>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
+          {isLoggedIn ? (
+            <>
+              <p>Welcome, {username}!</p>
+              <Link to="/logout">Logout</Link>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+            </>
+          )}
         </nav>
       </header>
 
